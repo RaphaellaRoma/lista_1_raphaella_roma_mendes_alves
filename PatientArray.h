ced45c58cc474bd4e9d690ca4e68973c533a3efb
Raphaella Roma Mendes Alves <162490655+RaphaellaRoma@users.noreply.github.com>
2025-03-12 15:40:10 -0300

    Adicionei as Structs Patient e PatientArray, além de declarar a função de inicialização do array

--- PatientArray.h
+++ PatientArray.h
@@ -1,1 +1,21 @@
+#ifndef PATIENT_ARRAY_H
+#define PATIENT_ARRAY_H
 
+namespace PatientArray{
+
+struct Patient {
+char name[50];
+int severity;
+char arrival_time[6];
+};
+
+struct PatientArray {
+  Patient *patients;
+  int size;
+  int capacity;
+};
+
+PatientArray * initializePatientArray();
+} //namespace
+
+#endif
